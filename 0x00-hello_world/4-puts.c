1a75d83e7dd529606922a098ce44f137b3e0e9a1
IB152000 <gomez.ihssane@gmail.com>
2023-06-15 19:28:01 -0700

    Update puts c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+/**
+ ** main - A PROGRAM THAT PRINTS A LINE WITH PUTS FUNCTION
+ * Return:0 (success)
+ */
+
 int main (void) {
-puts("\" Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
