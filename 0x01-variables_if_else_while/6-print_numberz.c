46d24b684964f76d9e612e2e1acc56264294d9ff
IB152000 <gomez.ihssane@gmail.com>
2023-06-18 16:43:58 -0700

    Update printNumberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,3 +5,16 @@
 * main - A program that prints the numbers
 * Return: 0 (Success)
 */
+
+int main(void)
+{
+int c;
+c = 0;
+while (c < 10)
+{
+putchar(c + '0');
+c++;
+}
+putchar('\n');
+return (0);
+}
